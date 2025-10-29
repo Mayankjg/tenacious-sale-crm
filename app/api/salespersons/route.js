5a74cd6d2a292ba0667778b59b2550d4fb83c49a
Mayankjg <24bmiitmscit005@gmail.com>
2025-10-29 09:57:51 +0530

    commit

--- app/api/salespersons/route.js
+++ app/api/salespersons/route.js
@@ -1,16 +1,3 @@
-// let salespersons = []; // temporary in-memory store
-
-// export async function GET() {
-//   return Response.json(salespersons);
-// }
-
-// export async function POST(req) {
-//   const body = await req.json();
-//   const newSalesperson = { id: Date.now(), ...body };
-//   salespersons.push(newSalesperson);
-//   return Response.json({ success: true, data: newSalesperson });
-// }
-
 let salespersons = [];
 
 export async function GET() {
--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,3 @@
-// import "./globals.css";
-
-// export const metadata = {
-//   title: "Tenacious Sales Dashboard",
-//   description: "CRM Interface built with Next.js and Tailwind CSS",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body>{children}</body>
-//     </html>
-//   );
-// }
-
 import "./globals.css";
 import LayoutClient from "../components/LayoutClient";
 
